dd86177de8c220104dfd2d08e919bb37db82651c
Neeraj Prasad <nprasad@mit.edu>
2019-01-17 16:44:18 -0500

    no synchr

--- tools/alt.py
+++ tools/alt.py
@@ -5,15 +5,14 @@ from os.path import isfile, join
 
 def main():
     homeDir = "/home/nprasad/Documents/github/maskrcnn-benchmark"
-    model_paths = ["../pretrained_models/fastercnnmodel_no_last_layers.pth"] + get_model_paths(join(homeDir, cfg.OUTPUT_DIR))
+    cfg = getCFG()
+    model_paths = [cfg.MODEL.WEIGHT] + get_model_paths(join(homeDir, cfg.OUTPUT_DIR))
     output = {}
     for path in model_paths:
     	ite, output = run(model_path)
     	output[ite] = output
 
     from maskrcnn_benchmark.engine.plotMaps import plot
-    from maskrcnn_benchmark.config import cfg
-    cfg.merge_from_file("/home/nprasad/Documents/github/maskrcnn-benchmark/configs/heads.yaml")
     plot(output, cfg)
 
 def run(model_path):
@@ -23,6 +22,12 @@ def run(model_path):
 def get_model_paths(directory):
     onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
     return [join(directory, file) for file in onlyfiles if ".pth" in file]
+
+def getCFG():
+	from maskrcnn_benchmark.config import cfg
+    cfg.merge_from_file("/home/nprasad/Documents/github/maskrcnn-benchmark/configs/heads.yaml")
+    return cfg
+
 
 if __name__ == "__main__":
 	main()
