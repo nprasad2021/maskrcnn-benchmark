60ea8aa8f226acbf804203fde712924d73717600
Neeraj Prasad <nprasad@mit.edu>
2019-01-24 12:20:49 -0500

    counter

--- tools/counter.py
+++ tools/counter.py
@@ -28,7 +28,7 @@ def run(args):
     )
     pil_image = Image.open(args.filename).convert("RGB")
     image = np.array(pil_image)[:, :, [2, 1, 0]]
-    cv2.imwrite(c.run_on_opencv_image(img), "../result.jpg")
+    cv2.imwrite(c.run_on_opencv_image(image), "../result.jpg")
     return c.get_count(image)
 
 def main():
