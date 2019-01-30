971320315648d235ead245b16fdb956a7dc6a3ac
Neeraj Prasad <nprasad@mit.edu>
2019-01-30 11:13:23 -0500

    lower batch sizes

--- maskrcnn_benchmark/engine/trainer.py
+++ maskrcnn_benchmark/engine/trainer.py
@@ -93,7 +93,7 @@ def do_train(
         eta_seconds = meters.time.global_avg * (max_iter - iteration)
         eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
 
-        if iteration % 200 == 0 or iteration == max_iter:
+        if iteration % 50 == 0 or iteration == max_iter:
             logger.info(
                 meters.delimiter.join(
                     [
